5ea4b4f9c28873d09605ffaf3c9aa85ebaabdb4a
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-08-17 17:28:11 -0500

    Creando schemas

---
+++ src/schemas/movieSchema.ts
@@ -0,0 +1,20 @@
+import { boolean, z } from "zod";
+
+export const MovieSchema = z.object({
+  adult: boolean(),
+  backdrop_path: z.string(),
+  id: z.number(),
+  original_language: z.string(),
+  original_title: z.string(),
+  overview: z.string(),
+  poster_path: z.string(),
+  release_date: z.string(),
+  title: z.string(),
+});
+
+export const MovieAPIResponse = z.object({
+  page: z.number(),
+  results: z.array(MovieSchema),
+  total_pages: z.number(),
+  total_results: z.number(),
+});
