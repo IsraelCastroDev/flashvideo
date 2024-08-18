25b37d613f6025b3457d39286173003b086f0ddf
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-17 22:11:37 -0500

    funcion para obtener una pelicula por su id

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,9 +1,23 @@
-import { MovieAPIResponse } from "../schemas/movieSchema";
+import { MovieAPIResponse, MovieSchema } from "../schemas/movieSchema";
+import { Movie } from "../types";
 import { api } from "./axios";
 
-export const getLatestMovies = async () => {
-  const { data } = await api("/movie/popular");
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const getPopularMovies = async () => {
+  const { data } = await api(
+    `/movie/popular?api_key=${API_KEY}&language=es-MX`
+  );
   const validateData = MovieAPIResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getMovieById = async (id: Movie["id"]) => {
+  const { data } = await api(`/movie/${id}?api_key=${API_KEY}&language=es-MX`);
+  const validateData = MovieSchema.safeParse(data);
 
   if (validateData.success) {
     return validateData.data;
