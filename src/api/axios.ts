0b2bea54487e74089a755bf60198511238554309
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-17 22:11:21 -0500

    Ajustando datos de la instancia de axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-
 export const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
-  headers: {
-    Authorization: `Bearer ${API_KEY}`,
-  },
 });
