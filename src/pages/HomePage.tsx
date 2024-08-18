b6cf9bcc1d25bac0882624426644dd1ecc57e1cf
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-17 22:12:55 -0500

    Usando useQuery para obtener los datos y renderizando el componente Carousel

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPopularMovies } from "../api/movies";
+import Carousel from "../components/ui/Carousel";
+
 function HomePage() {
+  const { data, isLoading } = useQuery({
+    queryFn: getPopularMovies,
+    queryKey: ["popular"],
+  });
+
+  if (isLoading) return <p>Cargando...</p>;
+
+  const popularMovies = data?.results ?? [];
+  const hasMovies = popularMovies.length > 0;
+
   return (
     <>
       <section>
         <h2 className="text-2xl font-bold">Películas populares</h2>
+
+        <ul className="mt-5">
+          {hasMovies ? (
+            <Carousel movies={popularMovies} />
+          ) : (
+            <p>No hay pelis</p>
+          )}
+        </ul>
       </section>
     </>
   );
