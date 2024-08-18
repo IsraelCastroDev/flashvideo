788c334cb1cad6f3d94b2063fbb2f22fb1766993
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-18 01:00:25 -0500

    agregando un segundo type guard

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,14 +1,18 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Cast, Movie } from "../../types";
+import { Cast, Movie, RecommendedMovie } from "../../types";
 import { Link } from "react-router-dom";
 import "swiper/css";
 
 interface Props {
-  data: Movie[] | Cast[];
+  data: Movie[] | Cast[] | RecommendedMovie[];
 }
 
-function isMovie(data: Movie | Cast): data is Movie {
-  return (data as Movie).title !== undefined;
+function isMovie(item: Movie | Cast | RecommendedMovie): item is Movie {
+  return (item as Movie).title !== undefined;
+}
+
+function isCast(item: Movie | Cast | RecommendedMovie): item is Cast {
+  return (item as Cast).known_for_department !== undefined;
 }
 
 function Carousel({ data }: Props) {
@@ -23,23 +27,48 @@ function Carousel({ data }: Props) {
         <SwiperSlide
           key={item.id}
           tag="li"
-          className={`${isMovie(item) ? "" : "bg-white h-52"}`}
+          className={
+            isMovie(item)
+              ? ""
+              : isCast(item)
+              ? "bg-white h-52"
+              : "bg-gray-200 h-60"
+          }
         >
-          <Link to={`/peliculas/${item.id}`}>
+          <Link
+            to={`${
+              isMovie(item) ? `/peliculas/${item.id}` : `/persona/${item.id}`
+            }`}
+          >
             <img
               src={`${import.meta.env.VITE_IMAGE_URL}w200${
-                isMovie(item) ? item.poster_path : item.profile_path
+                isMovie(item)
+                  ? item.poster_path
+                  : isCast(item)
+                  ? item.profile_path
+                  : item.poster_path
               }`}
-              alt={`Poster de ${isMovie(item) ? item.title : item.name}`}
+              alt={`Poster de ${
+                isMovie(item)
+                  ? item.title
+                  : isCast(item)
+                  ? item.name
+                  : item.poster_path
+              }`}
               className="rounded-xl"
             />
-            <h2 className="text-sm font-bold">{`${
-              isMovie(item) ? item.title : item.original_name
-            }`}</h2>
+            <h2 className="text-sm font-bold">
+              {isMovie(item)
+                ? item.title
+                : isCast(item)
+                ? item.original_name
+                : item.title}
+            </h2>
           </Link>
         </SwiperSlide>
       ))}
     </Swiper>
   );
 }
+
 export default Carousel;
