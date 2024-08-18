a384fc8c2e730436fbca6c999f4a085f2b231c51
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-18 00:59:33 -0500

    infiriendo type de pelicula recomendada

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,10 @@
 import { z } from "zod";
-import { CastSchema, MovieSchema } from "../schemas/movieSchema";
+import {
+  CastSchema,
+  MovieSchema,
+  RecommendedMovieSchema,
+} from "../schemas/movieSchema";
 
 export type Movie = z.infer<typeof MovieSchema>;
 export type Cast = z.infer<typeof CastSchema>;
+export type RecommendedMovie = z.infer<typeof RecommendedMovieSchema>;
