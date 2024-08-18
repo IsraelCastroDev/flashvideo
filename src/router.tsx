ea4e0aef811ff39ef6f9511fa0f19132eb2a72f5
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-17 22:13:39 -0500

    Agregando las rutas

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import HomePage from "./pages/HomePage";
-import MoviesPage from "./pages/MoviesPage";
+import MoviePage from "./pages/MoviePage";
 import PeoplePage from "./pages/PeoplePage";
 import SeriesPage from "./pages/SeriesPage";
+import MoviesPage from "./pages/MoviesPage";
+import LayoutMovie from "./layouts/LayoutMovie";
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
           <Route path="/peliculas" element={<MoviesPage />} />
           <Route path="/series" element={<SeriesPage />} />
           <Route path="/gente" element={<PeoplePage />} />
+        </Route>
+
+        <Route element={<LayoutMovie />}>
+          <Route path="/peliculas/:id" element={<MoviePage />} />
         </Route>
       </Routes>
     </>
