cf97b3f9d4652705c3eb7543ced87b3ccfdd8b3b
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-18 00:03:15 -0500

    Creando schemas cast

--- src/schemas/movieSchema.ts
+++ src/schemas/movieSchema.ts
@@ -11,6 +11,14 @@ export const MovieSchema = z.object({
   release_date: z.string(),
   title: z.string(),
   vote_average: z.number(),
+  genres: z
+    .array(
+      z.object({
+        id: z.number(),
+        name: z.string(),
+      })
+    )
+    .optional(),
 });
 
 export const MovieAPIResponse = z.object({
@@ -18,4 +26,41 @@ export const MovieAPIResponse = z.object({
   results: z.array(MovieSchema),
   total_pages: z.number(),
   total_results: z.number(),
+});
+
+// cast
+
+export const CastSchema = z.object({
+  adult: z.boolean(),
+  gender: z.number(),
+  id: z.number(),
+  known_for_department: z.string(),
+  name: z.string(),
+  original_name: z.string(),
+  popularity: z.number(),
+  profile_path: z.string().nullable().optional(),
+  cast_id: z.number(),
+  character: z.string(),
+  credit_id: z.string(),
+  order: z.number(),
+});
+
+export const CreditsAPIResponse = z.object({
+  id: z.number(),
+  cast: z.array(CastSchema),
+  crew: z.array(
+    z.object({
+      adult: z.boolean(),
+      gender: z.number(),
+      id: z.number(),
+      known_for_department: z.string(),
+      name: z.string(),
+      original_name: z.string(),
+      popularity: z.number(),
+      profile_path: z.string().nullable().optional(),
+      credit_id: z.string(),
+      department: z.string(),
+      job: z.string(),
+    })
+  ),
 });
