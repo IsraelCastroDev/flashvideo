5ed8a6b8b8d4bc553c83b7a80dcc6193f2f53318
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-18 01:46:43 -0500

    funciones que hacen llamador a la api, movies

---
+++ src/api/moviesAPI.ts
@@ -0,0 +1,44 @@
+import {
+  CreditsAPIResponse,
+  MovieAPIResponse,
+  MovieSchema,
+  RecommendedMovieAPIResponse,
+} from "../schemas/movieSchema";
+import { Movie } from "../types";
+import { api } from "./axios";
+
+export const getPopularMovies = async () => {
+  const { data } = await api(`/movie/popular`);
+  const validateData = MovieAPIResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getMovieById = async (id: Movie["id"]) => {
+  const { data } = await api(`/movie/${id}`);
+  const validateData = MovieSchema.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getCreditsByMovie = async (id: Movie["id"]) => {
+  const { data } = await api(`/movie/${id}/credits`);
+  const validateData = CreditsAPIResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getMovieRecommendations = async (id: Movie["id"]) => {
+  const { data } = await api(`/movie/${id}/recommendations`);
+  const validateData = RecommendedMovieAPIResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
