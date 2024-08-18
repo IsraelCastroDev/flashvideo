147fa0aa7d1b22542e50180b7d031d368443020e
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-18 01:47:33 -0500

    creando type de persona

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,7 +4,11 @@ import {
   MovieSchema,
   RecommendedMovieSchema,
 } from "../schemas/movieSchema";
+import { PersonSchema } from "../schemas/personSchema";
 
 export type Movie = z.infer<typeof MovieSchema>;
 export type Cast = z.infer<typeof CastSchema>;
 export type RecommendedMovie = z.infer<typeof RecommendedMovieSchema>;
+
+// person
+export type Person = z.infer<typeof PersonSchema>;
