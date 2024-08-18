dcf616bb70ec0563328fff14c560891af967529c
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-18 01:48:03 -0500

    agregando query params a mi instancia de axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: import.meta.env.VITE_API_KEY,
+    language: "es-MX",
+  },
 });
