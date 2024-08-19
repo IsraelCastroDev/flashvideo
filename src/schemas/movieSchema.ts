a18464c9e9dae9e32e3bf7ea9136230ae86ee829
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-19 16:31:14 -0500

    haciendo un schema mas general para pelicula

--- src/schemas/movieSchema.ts
+++ src/schemas/movieSchema.ts
@@ -2,12 +2,13 @@ import { boolean, z } from "zod";
 
 export const MovieSchema = z.object({
   adult: boolean(),
-  backdrop_path: z.string(),
+  backdrop_path: z.string().nullable().optional(),
+  genre_ids: z.array(z.number()).nullable().optional(),
   id: z.number(),
   original_language: z.string(),
   original_title: z.string(),
   overview: z.string(),
-  poster_path: z.string(),
+  poster_path: z.string().nullable().optional(),
   release_date: z.string(),
   title: z.string(),
   vote_average: z.number(),
@@ -20,6 +21,10 @@ export const MovieSchema = z.object({
     )
     .nullable()
     .optional(),
+  vote_count: z.number().nullable().optional(),
+  character: z.string().nullable().optional(),
+  credit_id: z.string().nullable().optional(),
+  order: z.number().nullable().optional(),
 });
 
 export const MovieAPIResponse = z.object({
@@ -33,8 +38,8 @@ export const MovieAPIResponse = z.object({
 
 export const PersonSchema = z.object({
   adult: z.boolean(),
-  also_known_as: z.array(z.string()).default([]),
-  biography: z.string(),
+  also_known_as: z.array(z.string()).default([]).nullable().optional(),
+  biography: z.string().nullable().optional(),
   birthday: z.string(),
   deathday: z.string().nullable().optional(),
   gender: z.number(),
@@ -83,28 +88,25 @@ export const CreditsAPIResponse = z.object({
   ),
 });
 
-// peliculas recomendadas
-export const RecommendedMovieSchema = z.object({
-  backdrop_path: z.string().nullable().optional(),
-  id: z.number(),
-  title: z.string(),
-  original_title: z.string(),
-  overview: z.string(),
-  poster_path: z.string(),
-  media_type: z.string(),
-  adult: z.boolean(),
-  original_language: z.string(),
-  genre_ids: z.array(z.number()),
-  popularity: z.number(),
-  release_date: z.string(),
-  video: z.boolean(),
-  vote_average: z.number(),
-  vote_count: z.number(),
-});
-
 export const RecommendedMovieAPIResponse = z.object({
   page: z.number(),
-  results: z.array(RecommendedMovieSchema),
+  results: z.array(MovieSchema),
   total_pages: z.number(),
   total_results: z.number(),
+});
+
+export const MovieCreditsAPIResponse = z.object({
+  cast: z.array(MovieSchema),
+  id: z.number(),
+});
+
+// KEYWORDS
+export const KeyWordsSchemaAPIResponse = z.object({
+  id: z.number(),
+  keywords: z.array(
+    z.object({
+      id: z.number(),
+      name: z.string(),
+    })
+  ),
 });
