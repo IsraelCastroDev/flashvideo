e0d3f767a8f11aa0544e890fdcc7f0e3e8affba5
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-19 16:29:22 -0500

    creando funcion para obtener creditos de peliculas por persona

--- src/api/personAPI.ts
+++ src/api/personAPI.ts
@@ -1,10 +1,19 @@
-import { PersonSchema } from "../schemas/movieSchema";
+import { MovieCreditsAPIResponse, PersonSchema } from "../schemas/movieSchema";
 import { Person } from "../types";
 import { api } from "./axios";
 
 export const getDetailsByPerson = async (id: Person["id"]) => {
   const { data } = await api(`/person/${id}`);
   const validateData = PersonSchema.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getMovieCreditsFromPerson = async (id: Person["id"]) => {
+  const { data } = await api(`/person/${id}/movie_credits`);
+  const validateData = MovieCreditsAPIResponse.safeParse(data);
 
   if (validateData.success) {
     return validateData.data;
