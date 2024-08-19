411e7599d6bf37e0d19e2d63f99159a423388bbb
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-19 16:30:03 -0500

    ajustando tipos y renderizando

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,18 +1,18 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Cast, Movie, RecommendedMovie } from "../../types";
+import { CastMember, Movie } from "../../types";
 import { Link } from "react-router-dom";
 import "swiper/css";
 
 interface Props {
-  data: Movie[] | Cast[] | RecommendedMovie[];
+  data: Movie[] | CastMember[];
 }
 
-function isMovie(item: Movie | Cast | RecommendedMovie): item is Movie {
+function isMovie(item: Movie | CastMember): item is Movie {
   return (item as Movie).title !== undefined;
 }
 
-function isCast(item: Movie | Cast | RecommendedMovie): item is Cast {
-  return (item as Cast).known_for_department !== undefined;
+function isCast(item: Movie | CastMember): item is CastMember {
+  return (item as CastMember).known_for_department !== undefined;
 }
 
 function Carousel({ data }: Props) {
@@ -20,6 +20,7 @@ function Carousel({ data }: Props) {
     <Swiper
       spaceBetween={30}
       slidesPerView={3}
+      tag="ul"
       // onSlideChange={() => console.log("slide change")}
       // onSwiper={(swiper) => console.log(swiper)}
     >
@@ -27,13 +28,13 @@ function Carousel({ data }: Props) {
         <SwiperSlide
           key={item.id}
           tag="li"
-          className={
+          className={`${
             isMovie(item)
               ? ""
               : isCast(item)
-              ? "bg-white h-52"
+              ? "bg-white h-60"
               : "bg-gray-200 h-60"
-          }
+          } rounded-xl h-72`}
         >
           <Link
             to={`${
@@ -41,29 +42,45 @@ function Carousel({ data }: Props) {
             }`}
           >
             <img
-              src={`${import.meta.env.VITE_IMAGE_URL}w200${
+              src={`${
+                isCast(item)
+                  ? item.profile_path
+                    ? `${import.meta.env.VITE_IMAGE_URL}w200${
+                        item.profile_path
+                      }`
+                    : "/img/perfil-default.jpg"
+                  : `${
+                      item.poster_path !== null
+                        ? `${import.meta.env.VITE_IMAGE_URL}w200${
+                            item.poster_path
+                          }`
+                        : "https://img.freepik.com/free-photo/movie-background-collage_23-2149876006.jpg"
+                    }`
+              }
+              `}
+              /*
+              ${import.meta.env.VITE_IMAGE_URL}w200${
                 isMovie(item)
                   ? item.poster_path
                   : isCast(item)
                   ? item.profile_path
                   : item.poster_path
-              }`}
-              alt={`Poster de ${
-                isMovie(item)
-                  ? item.title
-                  : isCast(item)
-                  ? item.name
-                  : item.poster_path
-              }`}
+              }
+              */
+              alt={`Poster de ${isMovie(item) ? item.title : item.name}`}
               className="rounded-xl"
+              loading="lazy"
             />
-            <h2 className="text-sm font-bold">
-              {isMovie(item)
-                ? item.title
-                : isCast(item)
-                ? item.original_name
-                : item.title}
-            </h2>
+
+            <div className="p-2">
+              <h2
+                className="text-sm font-bold"
+                title={`${isMovie(item) ? item.title : item.original_name}`}
+              >
+                {isMovie(item) ? item.title : item.original_name}
+              </h2>
+              {isCast(item) && <p className="text-sm">{item.character}</p>}
+            </div>
           </Link>
         </SwiperSlide>
       ))}
