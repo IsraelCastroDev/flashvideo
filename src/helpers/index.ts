7c1135488dccc469c8fc6d67572085bb53327d43
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-19 18:50:32 -0500

    funcion para formatear fecha

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,12 @@
 export const getYear = (date: string): number => {
   return new Date(date).getFullYear();
 };
+
+export const formatDate = (date: string): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  return new Intl.DateTimeFormat("es-MX", options).format(new Date(date));
+};
