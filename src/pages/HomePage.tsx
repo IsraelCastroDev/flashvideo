176d76e3fe7180a1ef1b08c089ce28bd60c8704a
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-19 17:55:20 -0500

    creando mas secciones mostrando el carrusel

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,38 @@
-import { useQuery } from "@tanstack/react-query";
-import { getPopularMovies } from "../api/moviesAPI";
 import Carousel from "../components/ui/Carousel";
 import Loader from "../components/ui/Loader/Loader";
+import { useMovies } from "../hooks/movies/useMovies";
+import { toast } from "react-toastify";
 
 function HomePage() {
-  const { data, isLoading } = useQuery({
-    queryFn: getPopularMovies,
-    queryKey: ["popular"],
-  });
+  const { popularMoviesQuery, topRatedMoviesQuery, upcommingMoviesQuery } =
+    useMovies();
 
-  if (isLoading) return <Loader />;
+  const {
+    data: popularMovies,
+    isLoading: isLoadingPopularMovies,
+    isError: isErrorPopularMovies,
+  } = popularMoviesQuery;
 
-  const popularMovies = data?.results ?? [];
-  const hasMovies = popularMovies.length > 0;
+  const {
+    data: topRatedMovies,
+    isLoading: isLoadingTopRatedMovies,
+    isError: isErrorTopRatedMovies,
+  } = topRatedMoviesQuery;
+
+  const {
+    data: upcommingMovies,
+    isLoading: isLoadingUpcommingMovies,
+    isError: isErrorUpcommingMovies,
+  } = upcommingMoviesQuery;
+
+  if (
+    isLoadingPopularMovies ||
+    isLoadingTopRatedMovies ||
+    isLoadingUpcommingMovies
+  )
+    return <Loader />;
+  if (isErrorPopularMovies || isErrorTopRatedMovies || isErrorUpcommingMovies)
+    return toast.error("No se pudo cargar la información");
 
   return (
     <>
@@ -20,7 +40,29 @@ function HomePage() {
         <h2 className="text-2xl font-bold">Películas populares</h2>
 
         <ul className="mt-5">
-          {hasMovies ? <Carousel data={popularMovies} /> : <p>No hay pelis</p>}
+          {popularMovies?.results !== undefined ? (
+            <Carousel data={popularMovies.results} />
+          ) : (
+            <p>No hay pelis</p>
+          )}
+        </ul>
+      </section>
+
+      <section>
+        <h2 className="text-2xl font-bold">Películas con mejor calificación</h2>
+        <ul className="mt-5">
+          {topRatedMovies?.results !== undefined && (
+            <Carousel data={topRatedMovies.results} />
+          )}
+        </ul>
+      </section>
+
+      <section>
+        <h2 className="text-2xl font-bold">Próximos estrenos</h2>
+        <ul className="mt-5">
+          {upcommingMovies?.results !== undefined && (
+            <Carousel data={upcommingMovies.results} />
+          )}
         </ul>
       </section>
     </>
