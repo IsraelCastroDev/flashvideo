937005fca23abb0b90b892815263af8422663642
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-19 20:38:13 -0500

    corregir manejo de la funcion fomartDate

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,12 +1,16 @@
-export const getYear = (date: string): number => {
+export const getYear = (date: string) => {
   return new Date(date).getFullYear();
 };
 
 export const formatDate = (date: string): string => {
+  if (!date) return "";
+
+  const dateParam = new Date(date);
+
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "short",
     day: "numeric",
   };
-  return new Intl.DateTimeFormat("es-MX", options).format(new Date(date));
+  return new Intl.DateTimeFormat("es-MX", options).format(dateParam);
 };
