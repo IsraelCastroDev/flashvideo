3fb9b697bf182f628d223223945624af29e85c09
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-20 14:07:33 -0500

    Creando componente reultilizable de lite youtube

---
+++ src/components/ui/LiteYoutuve.tsx
@@ -0,0 +1,17 @@
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+import { Video } from "../../types";
+
+interface Props {
+  id: Video["key"];
+  title: Video["name"];
+}
+
+function LiteYoutuve({ id, title }: Props) {
+  return (
+    <div>
+      <LiteYouTubeEmbed id={id} title={title} />
+    </div>
+  );
+}
+export default LiteYoutuve;
