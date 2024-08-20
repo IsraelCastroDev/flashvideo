7fe98bbac1c86b462074d9dfccbd7b6445b0b5ef
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-19 20:38:34 -0500

    corrigiendo bug al mostrar fecha

--- src/pages/PersonPage/PersonPage.tsx
+++ src/pages/PersonPage/PersonPage.tsx
@@ -88,7 +88,9 @@ function PersonPage() {
               className="flex items-center gap-6 border border-t-gray-500 py-2 last-of-type:border-b-gray-500"
             >
               <p className="font-semibold flex-initial">
-                {movie.release_date ? getYear(movie.release_date) : "-----"}
+                {movie.release_date !== ""
+                  ? getYear(movie.release_date)
+                  : "-----"}
               </p>
               <div>
                 <h4 className="font-bold">{movie.title}</h4>
