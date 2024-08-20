75bdcf0e065d90fc9819f69db10ccf2712901297
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-20 14:08:49 -0500

    creando schema de video

--- src/schemas/movieSchema.ts
+++ src/schemas/movieSchema.ts
@@ -127,3 +127,22 @@ export const UpcommingMoviesAPISchema = z.object({
   results: z.array(MovieSchema),
   total_pages: z.number(),
 });
+
+// ----------VIDEOS
+export const VideoSchema = z.object({
+  iso_639_1: z.string(),
+  iso_3166_1: z.string(),
+  name: z.string(),
+  key: z.string(),
+  site: z.string(),
+  size: z.number(),
+  type: z.string(),
+  official: z.boolean(),
+  published_at: z.string(),
+  id: z.string(),
+});
+
+export const VideosAPIResponse = z.object({
+  id: z.number(),
+  results: z.array(VideoSchema),
+});
