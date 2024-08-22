f1a1f33ba4dbf704d74e9f84e7ec73ae218ffc4c
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-21 23:20:58 -0500

    funcion para buscar

---
+++ src/api/search.ts
@@ -0,0 +1,15 @@
+import { MultiAPIResponse } from "../schemas/movieSchema";
+import { api } from "./axios";
+
+export const getSearchMultiResults = async (query: string) => {
+  const { data } = await api(`/search/multi?query=${query}`);
+  const validateData = MultiAPIResponse.safeParse(data);
+
+  console.log("data-zod----->", validateData);
+
+  if (validateData.success) {
+    return validateData.data;
+  } else {
+    console.log(validateData.error.message.toString());
+  }
+};
