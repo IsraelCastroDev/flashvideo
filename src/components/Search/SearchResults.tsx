c7814a21f6494c95b00141d93388617125e602dc
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-22 15:25:03 -0500

    componetizando

---
+++ src/components/Search/SearchResults.tsx
@@ -0,0 +1,152 @@
+import { Link } from "react-router-dom";
+import { convertStringToSlug, formatDate } from "../../helpers";
+import { Movie, Multi, Person, SearchResult } from "../../types";
+import { ImageIcon } from "../ui/Icons";
+
+interface Props {
+  searchResult: Multi;
+  isMovie: (item: SearchResult) => item is Movie;
+  isPerson: (item: SearchResult) => item is Person;
+}
+
+function SearchResults({ searchResult, isMovie, isPerson }: Props) {
+  return (
+    <>
+      {searchResult?.results.map((res) => {
+        if (isMovie(res)) {
+          return (
+            <li
+              key={res.id}
+              className="flex items-center gap-2 w-full shadow-md md:py-3"
+            >
+              <div className="w-1/4 flex items-center justify-center">
+                <Link
+                  to={`/movie/${res.id}-${convertStringToSlug(res.title!)}`}
+                >
+                  {res.poster_path !== null ? (
+                    <img
+                      src={`${import.meta.env.VITE_IMAGE_URL}/w300${
+                        res.poster_path
+                      }`}
+                      alt={res.name!}
+                      className="block object-cover w-30 h-44"
+                    />
+                  ) : (
+                    <img
+                      src={`https://img.freepik.com/free-photo/movie-background-collage_23-2149876006.jpg`}
+                      alt={res.name!}
+                      className="block object-cover w-30 h-44"
+                    />
+                  )}
+                </Link>
+              </div>
+
+              <div className="w-3/4">
+                <div>
+                  <Link
+                    to={`/movie/${res.id}-${convertStringToSlug(res.title!)}`}
+                  >
+                    <h2 className="font-bold text-lg">{res.title}</h2>
+                  </Link>
+                  <small className="text-gray-500">
+                    {formatDate(res.release_date!)}
+                  </small>
+                </div>
+
+                <div>
+                  <p className="truncate-text text-gray-500">{res.overview}</p>
+                </div>
+              </div>
+            </li>
+          );
+        } else if (isPerson(res)) {
+          return (
+            <li
+              key={res.id}
+              className="flex items-center gap-2 w-full shadow-md md:py-3"
+            >
+              <div className="w-1/4 flex items-center justify-center">
+                <Link
+                  to={`/person/${res.id}-${convertStringToSlug(res.name!)}`}
+                >
+                  {res.profile_path !== null &&
+                  res.profile_path !== undefined ? (
+                    <img
+                      src={`${import.meta.env.VITE_IMAGE_URL}/w300${
+                        res.profile_path
+                      }`}
+                      alt={res.name!}
+                      className="block object-cover w-30 h-44"
+                    />
+                  ) : (
+                    <img
+                      src={`https://img.freepik.com/free-photo/movie-background-collage_23-2149876006.jpg`}
+                      alt={res.name!}
+                      className="block object-cover w-30 h-44"
+                    />
+                  )}
+                </Link>
+              </div>
+
+              <div className="w-3/4">
+                <div>
+                  <Link
+                    to={`/person/${res.id}-${convertStringToSlug(res.name!)}`}
+                  >
+                    <h2 className="font-bold text-lg">{res.name}</h2>
+                  </Link>
+                  <small className="text-gray-500">
+                    {res.also_known_as?.map((a) => a)}
+                  </small>
+                </div>
+
+                <div>
+                  <p className="truncate-text text-gray-500">{res.biography}</p>
+                </div>
+              </div>
+            </li>
+          );
+        } else {
+          return (
+            <li
+              key={res.id}
+              className="flex items-center gap-2 w-full shadow-md md:py-3"
+            >
+              <div className="w-1/4 flex items-center justify-center">
+                {res.poster_path !== null ? (
+                  <img
+                    src={`${
+                      import.meta.env.VITE_IMAGE_URL
+                    }/w300${res.poster_path!}`}
+                    alt={res.name!}
+                    className="block object-cover w-30 h-44"
+                  />
+                ) : (
+                  <ImageIcon
+                    classname="fill-gray-500"
+                    width="10rem"
+                    height="14rem"
+                  />
+                )}
+              </div>
+
+              <div className="w-3/4">
+                <div>
+                  <h2 className="font-bold text-lg">{res.original_name}</h2>
+                  <small className="text-gray-500">
+                    {formatDate(res.first_air_date!)}
+                  </small>
+                </div>
+
+                <div>
+                  <p className="truncate-text text-gray-500">{res.overview}</p>
+                </div>
+              </div>
+            </li>
+          );
+        }
+      })}
+    </>
+  );
+}
+export default SearchResults;
--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -3,7 +3,8 @@ import { toast } from "react-toastify";
 import Loader from "../../components/ui/Loader/Loader";
 import { useSearch } from "../../hooks/search/useSearch";
 import { Movie, Person, SearchResult } from "../../types";
-import { formatDate } from "../../helpers";
+import SearchResults from "../../components/Search/SearchResults";
+import AsideSeachResult from "../../components/Search/AsideSeachResult";
 
 function SearhPage() {
   const queryParam = useLocation().search;
@@ -27,88 +28,24 @@ function SearhPage() {
   if (isLoadingSearch) return <Loader />;
   if (isErrorSearch) toast.error("Ocurrió un error al cargar la información");
   if (!searchResult) return <p>No se encontró la información</p>;
-  console.log(searchResult);
 
   return (
     <>
-      <section>
-        <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {searchResult?.results.map((res) => {
-            if (isMovie(res)) {
-              return (
-                <>
-                  <li key={res.id} className="flex flex-col gap-2">
-                    <div className="w-full">
-                      <img
-                        src={`${
-                          import.meta.env.VITE_IMAGE_URL
-                        }/w300${res.poster_path!}`}
-                        alt={res.name!}
-                        className="block object-cover w-full"
-                      />
-                    </div>
+      <section className="flex flex-col md:flex-row gap-5">
+        <AsideSeachResult />
 
-                    <div>
-                      <p>{res.title}</p>
-                      <small>{formatDate(res.release_date!)}</small>
-                    </div>
-                  </li>
-                </>
-              );
-            } else if (isPerson(res)) {
-              return (
-                <li key={res.id}>
-                  <div className="w-full">
-                    {res.profile_path !== null ? (
-                      <img
-                        src={`${import.meta.env.VITE_IMAGE_URL}/w300${
-                          res.profile_path
-                        }`}
-                        alt={res.name!}
-                        className="block object-cover w-full"
-                      />
-                    ) : (
-                      <img
-                        src={`https://img.freepik.com/free-photo/movie-background-collage_23-2149876006.jpg`}
-                        alt={res.name!}
-                        className="block object-cover w-full"
-                      />
-                    )}
-                  </div>
-                  <h2>{res.name}</h2>
-                  <p>{res.also_known_as?.map((name) => name)}</p>
-                </li>
-              );
-            } else {
-              return (
-                <li key={res.id}>
-                  <div className="w-full">
-                    {res.poster_path !== null ||
-                    res.poster_path !== undefined ? (
-                      <img
-                        src={`${
-                          import.meta.env.VITE_IMAGE_URL
-                        }/w300${res.poster_path!}`}
-                        alt={res.name!}
-                        className="block object-cover w-full"
-                      />
-                    ) : (
-                      <img
-                        src={`https://img.freepik.com/free-photo/movie-background-collage_23-2149876006.jpg`}
-                        alt={res.name!}
-                        className="block object-cover w-full"
-                      />
-                    )}
-                  </div>
-
-                  <div>
-                    <p>{res.original_name}</p>
-                    <small>{formatDate(res.first_air_date!)}</small>
-                  </div>
-                </li>
-              );
-            }
-          })}
+        <ul className="flex flex-col gap-4 md:w-2/3">
+          {searchResult.results.length > 0 ? (
+            <SearchResults
+              searchResult={searchResult}
+              isMovie={isMovie}
+              isPerson={isPerson}
+            />
+          ) : (
+            <div className="h-[calc(100vh-237px)]">
+              <p className="text-lg font-bold">No se encontraron resultados.</p>
+            </div>
+          )}
         </ul>
       </section>
     </>
