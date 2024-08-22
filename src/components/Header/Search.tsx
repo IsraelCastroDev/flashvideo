0df4fe9307849cee2373ae8cc53832f5fc69cd55
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-21 23:21:23 -0500

    creando el formulario de busqueda

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,25 +1,17 @@
+import { SearchIcon } from "../ui/Icons";
+
 function Search() {
   return (
-    <div className="bg-bg-image bg-center bg-cover bg-no-repeat min-h-72 max-h-80 flex justify-center items-center">
-      <div className="p-5 md:p-10 space-y-2 md:space-y-5 h-full w-full">
-        <div>
-          <h2 className="text-5xl font-black">Te damos la bienvenida.</h2>
-          <h3 className="text-3xl font-bold">
-            Millones de películas, series y gente por descubrir. Explora ya.
-          </h3>
-        </div>
-
-        <div className="bg-white flex justify-between h-12 rounded-full mt-2 md:mt-20">
-          <input
-            type="text"
-            className="outline-none bg-transparent text-gray-700 placeholder:text-gray-400 h-full px-3 md:px-5 w-full"
-            placeholder="Buscar..."
-          />
-          <button className="bg-emerald-500 md:hover:text-black h-full px-7 rounded-full font-bold">
-            Buscar
-          </button>
-        </div>
-      </div>
+    <div className="flex items-center gap-2 w-full border border-b-gray-500 px-4 py-2 md:py-4 md:px-10 sticky top-0">
+      <SearchIcon classname="fill-slate-500" />
+      <input
+        type="text"
+        placeholder="Avengers"
+        className="w-full outline-none h-full placeholder:text-gray-400 text-gray-500"
+      />
+      <button className="text-gray-500 hover:text-gray-600 font-black">
+        X
+      </button>
     </div>
   );
 }
