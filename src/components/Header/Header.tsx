e8c719c1082d6dc1b4d8f237254ea1b4033dfb42
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-21 23:21:11 -0500

    ajustando renderizado

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
 import { Link, useLocation } from "react-router-dom";
 import { MenuIcon, SearchIcon } from "../ui/Icons";
 import styles from "./Header.module.css";
+import SearchHome from "./SearchHome";
 import Search from "./Search";
 
 function Header() {
   const location = useLocation();
   const isHome = location.pathname === "/";
+  const isSearchPage = location.pathname.split("/")[1] === "search";
 
   return (
     <header className={`${!isHome ? "sticky top-0" : ""} text-white z-10`}>
-      <div className="flex items-center justify-between gap-1 bg-sky-950 py-5 md:py-1 px-4 sticky top-0 z-20">
+      <div className="flex items-center justify-between gap-1 bg-sky-950 py-5 md:py-1 px-4 md:px-10 sticky top-0 z-20">
         <div className="flex items-center gap-4 md:flex-row-reverse">
           <label htmlFor="btn-menu" className="cursor-pointer block md:hidden">
             <MenuIcon />
@@ -39,7 +41,8 @@ function Header() {
         </button>
       </div>
 
-      {isHome && <Search />}
+      {isHome && <SearchHome />}
+      {isSearchPage && <Search />}
     </header>
   );
 }
