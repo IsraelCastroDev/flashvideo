52f6f3fa97b16c8b1786fe2ad3ed69931a273a9f
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-22 11:20:41 -0500

    ajustando grid

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -32,48 +32,81 @@ function SearhPage() {
   return (
     <>
       <section>
-        <ul className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
+        <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {searchResult?.results.map((res) => {
             if (isMovie(res)) {
               return (
-                <li key={res.id} className="flex flex-col gap-2">
-                  <div className="w-full">
-                    <img
-                      src={`${
-                        import.meta.env.VITE_IMAGE_URL
-                      }/w300${res.poster_path!}`}
-                      alt={res.name!}
-                      className="block object-cover w-full"
-                    />
-                  </div>
+                <>
+                  <li key={res.id} className="flex flex-col gap-2">
+                    <div className="w-full">
+                      <img
+                        src={`${
+                          import.meta.env.VITE_IMAGE_URL
+                        }/w300${res.poster_path!}`}
+                        alt={res.name!}
+                        className="block object-cover w-full"
+                      />
+                    </div>
 
-                  <div>
-                    <p>{res.title}</p>
-                    <small>{formatDate(res.release_date!)}</small>
-                  </div>
-
-                  <div>
-                    <p className="truncate">{res.overview}</p>
-                  </div>
-                </li>
+                    <div>
+                      <p>{res.title}</p>
+                      <small>{formatDate(res.release_date!)}</small>
+                    </div>
+                  </li>
+                </>
               );
             } else if (isPerson(res)) {
               return (
                 <li key={res.id}>
-                  <div>
-                    <img
-                      src={`${import.meta.env.VITE_IMAGE_URL}/w300${
-                        res.profile_path
-                      }`}
-                      alt=""
-                    />
+                  <div className="w-full">
+                    {res.profile_path !== null ? (
+                      <img
+                        src={`${import.meta.env.VITE_IMAGE_URL}/w300${
+                          res.profile_path
+                        }`}
+                        alt={res.name!}
+                        className="block object-cover w-full"
+                      />
+                    ) : (
+                      <img
+                        src={`https://img.freepik.com/free-photo/movie-background-collage_23-2149876006.jpg`}
+                        alt={res.name!}
+                        className="block object-cover w-full"
+                      />
+                    )}
                   </div>
                   <h2>{res.name}</h2>
                   <p>{res.also_known_as?.map((name) => name)}</p>
                 </li>
               );
             } else {
-              return <li key={res.id}>{res.name}</li>;
+              return (
+                <li key={res.id}>
+                  <div className="w-full">
+                    {res.poster_path !== null ||
+                    res.poster_path !== undefined ? (
+                      <img
+                        src={`${
+                          import.meta.env.VITE_IMAGE_URL
+                        }/w300${res.poster_path!}`}
+                        alt={res.name!}
+                        className="block object-cover w-full"
+                      />
+                    ) : (
+                      <img
+                        src={`https://img.freepik.com/free-photo/movie-background-collage_23-2149876006.jpg`}
+                        alt={res.name!}
+                        className="block object-cover w-full"
+                      />
+                    )}
+                  </div>
+
+                  <div>
+                    <p>{res.original_name}</p>
+                    <small>{formatDate(res.first_air_date!)}</small>
+                  </div>
+                </li>
+              );
             }
           })}
         </ul>
