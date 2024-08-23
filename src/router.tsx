50b031284d7994881c010d4c0a9c40293c3f1853
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-23 13:54:47 -0500

    creando mas rutas

--- src/router.tsx
+++ src/router.tsx
@@ -2,9 +2,15 @@ import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Loader from "./components/ui/Loader/Loader";
-import "react-toastify/dist/ReactToastify.css";
 import SearhPage from "./pages/SearchPage/SearchPage";
 import NotFound from "./pages/NotFound";
+import "react-toastify/dist/ReactToastify.css";
+import SearchPersonPage from "./pages/SearchPage/SearchPersonPage";
+import LayoutSearch from "./layouts/LayoutSearch";
+import SearchCollectionPage from "./pages/SearchPage/SearchCollectionPage";
+import SearchMoviePage from "./pages/SearchPage/SearchMoviePage";
+import SearchKeywordPage from "./pages/SearchPage/SearchKeywordPage";
+import SearchTvPage from "./pages/SearchPage/SearchTvPage";
 
 const HomePageLazy = lazy(() => import("./pages/HomePage"));
 const LayoutLazy = lazy(() => import("./layouts/Layout"));
@@ -25,9 +31,23 @@ function App() {
             <Route path="/peliculas" element={<MoviesPageLazy />} />
             <Route path="/series" element={<SeriesPageLazy />} />
             <Route path="/gente" element={<PeoplePageLazy />} />
-            <Route path="search" element={<SearhPage />} />
             <Route path="*" element={<NotFound />} />
           </Route>
+
+          <Route element={<LayoutSearch />}>
+            <Route path="/search" element={<SearhPage />} />
+            <Route path="/search/person" element={<SearchPersonPage />} />
+            <Route path="/search/movie" element={<SearchMoviePage />} />
+            <Route path="/search/company" element={<SearchPersonPage />} />
+            <Route path="/search/tv" element={<SearchTvPage />} />
+            <Route path="/search/keyword" element={<SearchKeywordPage />} />
+            <Route
+              path="/search/collection"
+              element={<SearchCollectionPage />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+
           <Route element={<LayoutMovieLazy />}>
             <Route path="/movie/:id" element={<MoviePageLazy />} />
             <Route path="/person/:id" element={<PersonPageLazy />} />
@@ -35,7 +55,7 @@ function App() {
           </Route>
         </Routes>
       </Suspense>
-      <ToastContainer />
+      <ToastContainer pauseOnHover={false} closeOnClick={true} />
     </>
   );
 }
