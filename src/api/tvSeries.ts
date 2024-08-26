15fa36e5aa124a2aba5372d4145c88947aa9be23
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-26 16:27:11 -0500

    creando funciones para obtener info sobre serie

--- src/api/tvSeries.ts
+++ src/api/tvSeries.ts
@@ -1,12 +1,51 @@
-import { TVSeriesSchema } from "../schemas/movieSchema";
+import {
+  CreditsAPIResponse,
+  TVSerieKeywordsResponse,
+  TVSerieRecommendationsAPIResponse,
+  TVSeriesSchema,
+  VideosAPIResponse,
+} from "../schemas/movieSchema";
 import { api } from "./axios";
 
 export const getTVSerie = async (id: number) => {
   const { data } = await api(`/tv/${id}`);
   const validateData = TVSeriesSchema.safeParse(data);
 
-  console.log(data);
-  console.log(validateData);
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getTVSerieCredits = async (id: number) => {
+  const { data } = await api(`/tv/${id}/credits`);
+  const validateData = CreditsAPIResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getTVSerieVideos = async (id: number) => {
+  const { data } = await api(`/tv/${id}/videos`);
+  const validateData = VideosAPIResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getTVSerieRecommendations = async (id: number) => {
+  const { data } = await api(`/tv/${id}/recommendations`);
+  const validateData = TVSerieRecommendationsAPIResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getTVSerieKeywords = async (id: number) => {
+  const { data } = await api(`/tv/${id}/keywords`);
+  const validateData = TVSerieKeywordsResponse.safeParse(data);
 
   if (validateData.success) {
     return validateData.data;
