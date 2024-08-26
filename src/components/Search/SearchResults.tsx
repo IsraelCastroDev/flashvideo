9fe3cd11aca0892f4dff7b6fd302f4e56c8757fa
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-26 17:21:18 -0500

    agregando un type guard para collection

--- src/components/Search/SearchResults.tsx
+++ src/components/Search/SearchResults.tsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
 import { convertStringToSlug, formatDate } from "../../helpers";
-import { Movie, Multi, Person, SearchResult } from "../../types";
+import { Collection, Movie, Multi, Person, SearchResult } from "../../types";
 import { ImageIcon } from "../ui/Icons";
 
 interface Props {
   searchResult: Multi;
-  isMovie: (item: SearchResult) => item is Movie;
-  isPerson: (item: SearchResult) => item is Person;
 }
 
-function SearchResults({ searchResult, isMovie, isPerson }: Props) {
+const isMovie = (item: SearchResult): item is Movie => {
+  return "original_title" in item; // devuelve true o false
+};
+
+const isPerson = (item: SearchResult): item is Person => {
+  return "profile_path" in item;
+};
+
+const isCollectionMovies = (item: SearchResult): item is Collection => {
+  return "original_name" in item;
+};
+
+function SearchResults({ searchResult }: Props) {
   return (
     <>
       {searchResult?.results.map((res) => {
@@ -101,6 +111,56 @@ function SearchResults({ searchResult, isMovie, isPerson }: Props) {
 
                 <div>
                   <p className="truncate-text text-gray-500">{res.biography}</p>
+                </div>
+              </div>
+            </li>
+          );
+        } else if (isCollectionMovies(res)) {
+          return (
+            <li
+              key={res.id}
+              className="flex items-center gap-2 w-full shadow-md md:py-3"
+            >
+              <div className="w-1/4 flex items-center justify-center">
+                <Link
+                  to={`/collection/${res.id}-${convertStringToSlug(
+                    res.original_name!
+                  )}`}
+                >
+                  {res.poster_path !== null ? (
+                    <img
+                      src={`${import.meta.env.VITE_IMAGE_URL}/w300${
+                        res.poster_path
+                      }`}
+                      alt={res.name!}
+                      className="block object-cover w-30 h-44"
+                    />
+                  ) : (
+                    <ImageIcon
+                      classname="fill-gray-500"
+                      width="10rem"
+                      height="14rem"
+                    />
+                  )}
+                </Link>
+              </div>
+
+              <div className="w-3/4">
+                <div>
+                  <Link
+                    to={`/movie/${res.id}-${convertStringToSlug(
+                      res.original_name!
+                    )}`}
+                  >
+                    <h2 className="font-bold text-lg">{res.original_name}</h2>
+                  </Link>
+                  {/* <small className="text-gray-500">
+                    {formatDate(res.!)}
+                  </small> */}
+                </div>
+
+                <div>
+                  <p className="truncate-text text-gray-500">{res.overview}</p>
                 </div>
               </div>
             </li>
