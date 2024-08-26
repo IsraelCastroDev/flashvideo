355fedaf0d05e62868e19af842bf1e56d2f6ad0e
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-26 17:20:46 -0500

    corrigiendo resaltado en modo mobile

--- src/components/Search/AsideSeachResult.tsx
+++ src/components/Search/AsideSeachResult.tsx
@@ -20,7 +20,9 @@ function AsideSeachResult({ query }: Props) {
             <NavLink
               to={`/search/tv?query=${query}`}
               className={({ isActive }) =>
-                `block w-full h-full ${isActive ? "md:bg-gray-300" : ""} md:p-3`
+                `block w-full h-full ${
+                  isActive ? "text-sky-600 md:bg-gray-300 md:text-gray-800" : ""
+                } md:p-3`
               }
             >
               Series
@@ -30,7 +32,9 @@ function AsideSeachResult({ query }: Props) {
             <NavLink
               to={`/search/movie?query=${query}`}
               className={({ isActive }) =>
-                `block h-full w-full ${isActive ? "md:bg-gray-300" : ""} md:p-3`
+                `block h-full w-full ${
+                  isActive ? "text-sky-600 md:bg-gray-300 md:text-gray-800" : ""
+                } md:p-3`
               }
             >
               Películas
@@ -40,7 +44,9 @@ function AsideSeachResult({ query }: Props) {
             <NavLink
               to={`/search/collection?query=${query}`}
               className={({ isActive }) =>
-                `block h-full w-full ${isActive ? "md:bg-gray-300" : ""} md:p-3`
+                `block h-full w-full ${
+                  isActive ? "text-sky-600 md:bg-gray-300 md:text-gray-800" : ""
+                } md:p-3`
               }
             >
               Colecciones
@@ -50,7 +56,9 @@ function AsideSeachResult({ query }: Props) {
             <NavLink
               to={`/search/keyword?query=${query}`}
               className={({ isActive }) =>
-                `block h-full w-full ${isActive ? "md:bg-gray-300" : ""} md:p-3`
+                `block h-full w-full ${
+                  isActive ? "text-sky-600 md:bg-gray-300 md:text-gray-800" : ""
+                } md:p-3`
               }
             >
               Palabras Claves
@@ -60,7 +68,9 @@ function AsideSeachResult({ query }: Props) {
             <NavLink
               to={`/search/person?query=${query}`}
               className={({ isActive }) =>
-                `block h-full w-full ${isActive ? "md:bg-gray-300" : ""} md:p-3`
+                `block h-full w-full ${
+                  isActive ? "text-sky-600 md:bg-gray-300 md:text-gray-800" : ""
+                } md:p-3`
               }
             >
               Gente
