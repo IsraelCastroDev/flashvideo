59b32f7176a7ab924d0ef827403afa058177e09c
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-28 16:56:07 -0500

    funcion para obtener datos de la coleccion

---
+++ src/api/collection.ts
@@ -0,0 +1,11 @@
+import { CollectionSchema } from "../schemas/movieSchema";
+import { api } from "./axios";
+
+export const getCollection = async (id: number) => {
+  const { data } = await api(`/collection/${id}`);
+  const validateData = CollectionSchema.safeParse(data);
+
+  if (validateData) {
+    return validateData.data;
+  }
+};
