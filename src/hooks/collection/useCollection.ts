008dc7d70a48ed5ab601cd754850db38dc71be39
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-28 16:55:25 -0500

    hook para collection

---
+++ src/hooks/collection/useCollection.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCollection } from "../../api/collection";
+
+export function useCollection(id: number) {
+  const collectionQuery = useQuery({
+    queryKey: ["collection", id],
+    queryFn: async () => {
+      if (id === undefined) return Promise.resolve(null);
+      const data = await getCollection(id);
+      return {
+        ...data,
+        id: id ?? 0,
+        type_identifier: "collection",
+        adult: data?.adult ?? false,
+        name: data?.name ?? "",
+      };
+    },
+    enabled: id !== undefined,
+  });
+
+  return { collectionQuery };
+}
