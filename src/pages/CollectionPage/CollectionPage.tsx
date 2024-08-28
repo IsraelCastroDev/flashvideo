2f258b2706691d377058e1e3591c9a43a2f9e195
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-28 16:57:46 -0500

    usando la informacion

--- src/pages/CollectionPage/CollectionPage.tsx
+++ src/pages/CollectionPage/CollectionPage.tsx
@@ -1,4 +1,40 @@
+import { toast } from "react-toastify";
+import MovieDetails from "../../components/PageComponents/Movie/MovieDetails";
+import RelatedInformationAboutMovie from "../../components/PageComponents/Movie/RelatedInformationAboutMovie";
+import { useCollection } from "../../hooks/collection/useCollection";
+import Loader from "../../components/ui/Loader/Loader";
+import { useParams } from "react-router-dom";
+
 function CollectionPage() {
-  return <div>CollectionPage</div>;
+  const { id } = useParams<{ id?: string }>();
+  const idMovieSplit = id?.split("-")[0];
+
+  const movieId = idMovieSplit !== undefined ? Number(idMovieSplit) : 0;
+
+  const { collectionQuery } = useCollection(movieId);
+
+  const {
+    data: collectionData,
+    isLoading: isLoadingCollection,
+    isError: isErrorCollection,
+  } = collectionQuery;
+
+  if (isLoadingCollection) return <Loader />;
+  if (isErrorCollection)
+    return toast.error("No se ha podido cargar la colección");
+  if (!collectionData)
+    return toast.error("No se ha podido cargar la colección");
+
+  return (
+    <>
+      {collectionData && (
+        <>
+          <MovieDetails movie={collectionData} />
+
+          <RelatedInformationAboutMovie movie={collectionData} />
+        </>
+      )}
+    </>
+  );
 }
 export default CollectionPage;
