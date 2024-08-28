4e245e8e02d8208928cad317f755f82b61a981d0
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-28 13:01:28 -0500

    creando types con el campo type_identifier

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,11 +14,27 @@ import {
   TVSerieRecommendationsAPIResponse,
   TVSerieKeywordsResponse,
   CollectionSchema,
+  MultiAPIResponseWithType,
+  PersonSchemaWithType,
+  CollectionSchemaWithType,
+  TVSeriesSchemaWithType,
+  CastMemberSchemaWithType,
 } from "../schemas/movieSchema";
 
 export type Movie = z.infer<typeof MovieSchema>;
+export interface MovieWithType extends Movie {
+  type_identifier: string;
+}
+
 export type Collection = z.infer<typeof CollectionSchema>;
+export type CollectionWithType = z.infer<typeof CollectionSchemaWithType>;
+
+export type TVSerie = z.infer<typeof TVSeriesSchema>;
+export type TVSerieWithType = z.infer<typeof TVSeriesSchemaWithType>;
+
 export type CastMember = z.infer<typeof CastMemberSchema>;
+export type CastMemberWithType = z.infer<typeof CastMemberSchemaWithType>;
+
 export type MovieCredits = z.infer<typeof MovieCreditsAPIResponse>;
 export type CreditsResponse = z.infer<typeof CreditsAPIResponse>;
 export type RecommendationsResponse = z.infer<
@@ -33,14 +49,18 @@ export type TVSerieKeywordsResponse = z.infer<typeof TVSerieKeywordsResponse>;
 
 // person
 export type Person = z.infer<typeof PersonSchema>;
+export type PersonWithType = z.infer<typeof PersonSchemaWithType>;
 
 // video
 export type Video = z.infer<typeof VideoSchema>;
 
 // search
 export type Multi = z.infer<typeof MultiAPIResponse>;
+export type MultiWithType = z.infer<typeof MultiAPIResponseWithType>;
+export type SearchResult =
+  | MovieWithType
+  | PersonWithType
+  | CollectionWithType
+  | TVSerieWithType;
 
-export type TVSerie = z.infer<typeof TVSeriesSchema>;
 export type Keyword = z.infer<typeof KeywordSchema>;
-
-export type SearchResult = Movie | Person | TVSerie;
