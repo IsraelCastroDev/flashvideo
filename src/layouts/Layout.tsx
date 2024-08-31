c5c80309cef5b1b40fa01af41ad45342a77aec47
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-31 17:59:31 -0500

    actualizando importacion

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
-import Header from "../components/PageComponents/Header/Header";
+import Header from "../components/ui/Header/Header";
 
 function Layout() {
   return (
