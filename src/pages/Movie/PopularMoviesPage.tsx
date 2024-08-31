9db46e401ba269259b27099dfd56f3ff5f478308
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-31 18:00:04 -0500

    ajustando grid

--- src/pages/Movie/PopularMoviesPage.tsx
+++ src/pages/Movie/PopularMoviesPage.tsx
@@ -12,9 +12,9 @@ function MoviesPage() {
   if (isError) return toast.error("No se pudo cargar la información");
 
   return (
-    <section>
+    <section className="flex-1">
       {popularMovies && (
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <ul className="grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-5">
           {popularMovies?.results.length > 0 ? (
             popularMovies?.results.map((movie) => <MovieCard movie={movie} />)
           ) : (
