95386711a3305196fb6973d063d5f57cd03b3950
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-31 17:59:06 -0500

    ajustando estilos

--- src/components/PageComponents/Movie/MovieCard.tsx
+++ src/components/PageComponents/Movie/MovieCard.tsx
@@ -1,25 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { convertStringToSlug, formatDate } from "../../../helpers";
 import { Movie } from "../../../types";
 import { ImageIcon } from "../../ui/Icons";
+import { useMemo } from "react";
 
 interface Props {
   movie: Movie;
 }
 
 function MovieCard({ movie }: Props) {
+  const pathName = useLocation().pathname;
+  const isSearchPage = useMemo(() => pathName === "/movie", [pathName]);
+
   return (
     <li
       key={movie.id}
-      className="flex items-center gap-2 w-full shadow-md md:py-3"
+      className={`flex items-center ${
+        isSearchPage ? "flex-col" : ""
+      } gap-2 w-full shadow-md md:py-3`}
     >
-      <div className="w-1/4 flex items-center justify-center">
+      <div
+        className={`flex items-center ${
+          isSearchPage ? "" : "justify-center w-1/4 "
+        }`}
+      >
         <Link to={`/movie/${movie.id}-${convertStringToSlug(movie.title!)}`}>
           {movie.poster_path !== null ? (
             <img
               src={`${import.meta.env.VITE_IMAGE_URL}/w300${movie.poster_path}`}
               alt={movie.name!}
-              className="block object-cover w-30 h-44"
+              className={`block object-cover ${
+                isSearchPage ? "w-full" : "w-30 h-44"
+              }`}
             />
           ) : (
             <ImageIcon classname="fill-gray-500" width="10rem" height="14rem" />
@@ -37,9 +49,11 @@ function MovieCard({ movie }: Props) {
           </small>
         </div>
 
-        <div>
-          <p className="truncate-text text-gray-500">{movie.overview}</p>
-        </div>
+        {!isSearchPage && (
+          <div>
+            <p className="truncate-text text-gray-500">{movie.overview}</p>
+          </div>
+        )}
       </div>
     </li>
   );
