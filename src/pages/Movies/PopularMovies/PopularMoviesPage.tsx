731e7d5320178f24f5d423abfaf48ce1dc02eb91
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-31 19:08:57 -0500

    importando el hook que tiene la logica de los filtros

--- src/pages/Movies/PopularMovies/PopularMoviesPage.tsx
+++ src/pages/Movies/PopularMovies/PopularMoviesPage.tsx
@@ -1,22 +1,27 @@
 import { toast } from "react-toastify";
-import { useMovies } from "../../hooks/movies/useMovies";
-import Loader from "../../components/ui/Loader/Loader";
-import MovieCard from "../../components/PageComponents/Movie/MovieCard";
+import { useMovies } from "../../../hooks/movies/useMovies";
+import Loader from "../../../components/ui/Loader/Loader";
+import MovieCard from "../../../components/PageComponents/Movie/MovieCard";
+import { useFiltersMovies } from "../../../hooks/filters/useFiltersMovies";
 
 function MoviesPage() {
   const { popularMoviesQuery } = useMovies();
 
   const { data: popularMovies, isLoading, isError } = popularMoviesQuery;
 
+  const { filteredPopularMovies } = useFiltersMovies(popularMovies?.results);
+
   if (isLoading) return <Loader />;
   if (isError) return toast.error("No se pudo cargar la información");
 
   return (
     <section className="flex-1">
-      {popularMovies && (
+      {filteredPopularMovies && (
         <ul className="grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-5">
-          {popularMovies?.results.length > 0 ? (
-            popularMovies?.results.map((movie) => <MovieCard movie={movie} />)
+          {filteredPopularMovies.length > 0 ? (
+            filteredPopularMovies.map((movie) => (
+              <MovieCard movie={movie} key={movie.id} />
+            ))
           ) : (
             <li>No se encontró la información</li>
           )}
