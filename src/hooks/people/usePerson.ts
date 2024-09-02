a2bb20124460c0748b7e6738361677e2b7b855e3
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 16:36:55 -0500

    ajustando el hook para ejecutar query que no necesiten el id

--- src/hooks/people/usePerson.ts
+++ src/hooks/people/usePerson.ts
@@ -1,47 +1,11 @@
-import { useQueries } from "@tanstack/react-query";
-import { Person } from "../../types";
-import {
-  getDetailsByPerson,
-  getMovieCreditsFromPerson,
-} from "../../api/personAPI";
-import { addTypeToResults } from "../../helpers";
+import { useQuery } from "@tanstack/react-query";
+import { getPopularPerson } from "../../api/personAPI";
 
-export function usePerson(personId: Person["id"]) {
-  const result = useQueries({
-    queries: [
-      {
-        queryKey: ["person", personId],
-        queryFn: async () => {
-          if (personId === undefined) return Promise.resolve(null);
-          const data = await getDetailsByPerson(personId);
-          return {
-            ...data,
-            type_identifier: "person",
-            id: personId ?? 0,
-            adult: data?.adult ?? false,
-            name: data?.name ?? "",
-          };
-        },
-        enabled: personId !== undefined,
-      },
-      {
-        queryKey: ["movieCreditsFromPerson", personId],
-        queryFn: async () => {
-          if (personId === undefined) return Promise.resolve(null);
-          const data = await getMovieCreditsFromPerson(personId);
-          const modifiedData = addTypeToResults(data!.cast, "movie");
-          return {
-            ...data,
-            cast: modifiedData,
-            id: personId ?? 0,
-          };
-        },
-        enabled: personId !== undefined,
-      },
-    ],
+export function usePerson() {
+  const popularPersonsQuery = useQuery({
+    queryKey: ["popularPersons"],
+    queryFn: getPopularPerson,
   });
 
-  const [personQuery, movieCreditsFromPersonQuery] = result;
-
-  return { personQuery, movieCreditsFromPersonQuery };
+  return { popularPersonsQuery };
 }
