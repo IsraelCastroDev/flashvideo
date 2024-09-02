367714aa3fe93401a82a4d079b808944acb4dc5b
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 12:12:42 -0500

    agregando el nuevo slice de genres

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
-import { sortSlice, FiltersSliceType } from "./filtersSlice";
+import { sortSlice, FiltersSliceType } from "./sortSlice";
+import { genresSlice, GenresSliceType } from "./genresSlice";
 
-export const useAppStore = create<FiltersSliceType>()(
+export const useAppStore = create<FiltersSliceType & GenresSliceType>()(
   devtools((...a) => ({
     ...sortSlice(...a),
+    ...genresSlice(...a),
   }))
 );
