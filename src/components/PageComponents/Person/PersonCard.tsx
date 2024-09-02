0e900aa32ffd2e7aa8eb96314862ce9094dccf07
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 16:34:51 -0500

    ajustando detalles del card de person

--- src/components/PageComponents/Person/PersonCard.tsx
+++ src/components/PageComponents/Person/PersonCard.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import { convertStringToSlug } from "../../../helpers";
+import { Link, useLocation } from "react-router-dom";
+import { convertStringToSlug, formatDate } from "../../../helpers";
 import { Person } from "../../../types";
 import { ImageIcon } from "../../ui/Icons";
 
@@ -8,20 +8,31 @@ interface Props {
 }
 
 function PersonCard({ person }: Props) {
+  const location = useLocation();
+  const isPersonPage = location.pathname.startsWith("/person");
+
   return (
     <li
       key={person.id}
-      className="flex items-center gap-2 w-full shadow-md md:py-3"
+      className={`flex items-center ${
+        isPersonPage ? "flex-col" : ""
+      } gap-2 w-full shadow-md md:py-3`}
     >
-      <div className="w-1/4 flex items-center justify-center">
-        <Link to={`/person/${person.id}-${convertStringToSlug(person.name!)}`}>
+      <div
+        className={`flex items-center ${
+          isPersonPage ? "" : "justify-center w-1/4 "
+        }`}
+      >
+        <Link to={`/movie/${person.id}-${convertStringToSlug(person.name!)}`}>
           {person.profile_path !== null ? (
             <img
               src={`${import.meta.env.VITE_IMAGE_URL}/w300${
                 person.profile_path
               }`}
               alt={person.name!}
-              className="block object-cover w-30 h-44"
+              className={`block object-cover ${
+                isPersonPage ? "w-full" : "w-30 h-44"
+              }`}
             />
           ) : (
             <ImageIcon classname="fill-gray-500" width="10rem" height="14rem" />
@@ -31,19 +42,19 @@ function PersonCard({ person }: Props) {
 
       <div className="w-3/4">
         <div>
-          <Link
-            to={`/person/${person.id}-${convertStringToSlug(person.name!)}`}
-          >
+          <Link to={`/movie/${person.id}-${convertStringToSlug(person.name!)}`}>
             <h2 className="font-bold text-lg">{person.name}</h2>
           </Link>
           <small className="text-gray-500">
-            {person.also_known_as?.map((a) => a)}
+            {formatDate(person.birthday!)}
           </small>
         </div>
 
-        <div>
-          <p className="truncate-text text-gray-500">{person.biography}</p>
-        </div>
+        {!isPersonPage && (
+          <div>
+            <p className="truncate-text text-gray-500">{person.biography}</p>
+          </div>
+        )}
       </div>
     </li>
   );
