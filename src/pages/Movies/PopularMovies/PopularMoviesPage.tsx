32b302d108f7a9ca94547bb64c21085eeea6098c
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 12:11:43 -0500

    actualizando importacion

--- src/pages/Movies/PopularMovies/PopularMoviesPage.tsx
+++ src/pages/Movies/PopularMovies/PopularMoviesPage.tsx
@@ -1,7 +1,7 @@
 import { toast } from "react-toastify";
 import { useMovies } from "../../../hooks/movies/useMovies";
-import Loader from "../../../components/ui/Loader/Loader";
 import MovieCard from "../../../components/PageComponents/Movie/MovieCard";
+import Loader from "../../../components/ui/Loader/Loader";
 import { useSortMovies } from "../../../hooks/filters/useSortMovies";
 
 function MoviesPage() {
@@ -19,9 +19,9 @@ function MoviesPage() {
       {filteredPopularMovies && (
         <ul className="grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-5">
           {filteredPopularMovies.length > 0 ? (
-            filteredPopularMovies.map((movie) => (
-              <MovieCard movie={movie} key={movie.id} />
-            ))
+            filteredPopularMovies.map((movie) => {
+              return <MovieCard movie={movie} key={movie.id} />;
+            })
           ) : (
             <li>No se encontró la información</li>
           )}
