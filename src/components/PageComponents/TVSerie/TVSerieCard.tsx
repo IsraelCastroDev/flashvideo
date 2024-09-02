fd02920f40fdcf344c2550e04e81d47084e10434
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 16:35:03 -0500

    ajustando detalles del card de tvserie

--- src/components/PageComponents/TVSerie/TVSerieCard.tsx
+++ src/components/PageComponents/TVSerie/TVSerieCard.tsx
@@ -1,27 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { TVSerie } from "../../../types";
 import { ImageIcon } from "../../ui/Icons";
 import { convertStringToSlug, formatDate } from "../../../helpers";
+import { useMemo } from "react";
 
 interface Props {
   tvSerie: TVSerie;
 }
 
 function TVSerieCard({ tvSerie }: Props) {
+  const pathName = useLocation().pathname;
+  const isTVSeriePage = useMemo(() => pathName.startsWith("/tv"), [pathName]);
+
   return (
     <li
       key={tvSerie.id}
-      className="flex items-center gap-2 w-full shadow-md md:py-3"
+      className={`flex items-center ${
+        isTVSeriePage ? "flex-col" : ""
+      } gap-2 w-full shadow-md md:py-3`}
     >
-      <div className="w-1/4 flex items-center justify-center">
-        <Link to={`/tv/${tvSerie.id}-${convertStringToSlug(tvSerie.name!)}`}>
+      <div
+        className={`flex items-center ${
+          isTVSeriePage ? "" : "justify-center w-1/4 "
+        }`}
+      >
+        <Link to={`/movie/${tvSerie.id}-${convertStringToSlug(tvSerie.name!)}`}>
           {tvSerie.poster_path !== null ? (
             <img
-              src={`${
-                import.meta.env.VITE_IMAGE_URL
-              }/w300${tvSerie.poster_path!}`}
+              src={`${import.meta.env.VITE_IMAGE_URL}/w300${
+                tvSerie.poster_path
+              }`}
               alt={tvSerie.name!}
-              className="block object-cover w-30 h-44"
+              className={`block object-cover ${
+                isTVSeriePage ? "w-full" : "w-30 h-44"
+              }`}
             />
           ) : (
             <ImageIcon classname="fill-gray-500" width="10rem" height="14rem" />
@@ -31,7 +43,9 @@ function TVSerieCard({ tvSerie }: Props) {
 
       <div className="w-3/4">
         <div>
-          <Link to={`/tv/${tvSerie.id}-${convertStringToSlug(tvSerie.name!)}`}>
+          <Link
+            to={`/movie/${tvSerie.id}-${convertStringToSlug(tvSerie.name!)}`}
+          >
             <h2 className="font-bold text-lg">{tvSerie.name}</h2>
           </Link>
           <small className="text-gray-500">
@@ -39,9 +53,11 @@ function TVSerieCard({ tvSerie }: Props) {
           </small>
         </div>
 
-        <div>
-          <p className="truncate-text text-gray-500">{tvSerie.overview}</p>
-        </div>
+        {!isTVSeriePage && (
+          <div>
+            <p className="truncate-text text-gray-500">{tvSerie.overview}</p>
+          </div>
+        )}
       </div>
     </li>
   );
