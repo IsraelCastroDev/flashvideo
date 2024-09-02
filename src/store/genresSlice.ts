004937d843ccb339cfeb6cbbd6e3224b14a9a47e
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 16:40:34 -0500

    creando un nuevo state para los generos de tvseries

--- src/store/genresSlice.ts
+++ src/store/genresSlice.ts
@@ -3,12 +3,18 @@ import { Genre } from "../types";
 
 export type GenresSliceType = {
   genres: Genre[];
+  genresTVSerie: Genre[];
   setGenres: (genres: Genre[]) => void;
+  setGenresTVSeries: (genresTVSerie: Genre[]) => void;
 };
 
 export const genresSlice: StateCreator<GenresSliceType> = (set) => ({
   genres: [],
+  genresTVSerie: [],
   setGenres: (genres) => {
     set({ genres });
+  },
+  setGenresTVSeries: (genresTVSerie) => {
+    set({ genresTVSerie });
   },
 });
