42ac2a8e6d546df638d49863a6a0a00ba49f01eb
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 14:38:34 -0500

    creando las demas paginas

---
+++ src/pages/Movies/TopRatedMovies/TopRatedMoviesPage.tsx
@@ -0,0 +1,34 @@
+import { toast } from "react-toastify";
+import Loader from "../../../components/ui/Loader/Loader";
+import { useFilterTopRatedMovies } from "../../../hooks/filters/useSiltersMovies";
+import { useMovies } from "../../../hooks/movies/useMovies";
+import MovieCard from "../../../components/PageComponents/Movie/MovieCard";
+
+function TopRatedMoviesPage() {
+  const { topRatedMoviesQuery } = useMovies();
+  const { data: topRatedMovies, isLoading, isError } = topRatedMoviesQuery;
+
+  const { filteredTopRatedMovies } = useFilterTopRatedMovies(
+    topRatedMovies?.results
+  );
+
+  if (isLoading) return <Loader />;
+  if (isError) return toast.error("No se pudo cargar la información");
+
+  return (
+    <section className="flex-1">
+      {filteredTopRatedMovies && (
+        <ul className="grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-5">
+          {filteredTopRatedMovies.length > 0 ? (
+            filteredTopRatedMovies.map((movie) => {
+              return <MovieCard movie={movie} key={movie.id} />;
+            })
+          ) : (
+            <li>No se encontró la información</li>
+          )}
+        </ul>
+      )}
+    </section>
+  );
+}
+export default TopRatedMoviesPage;
---
+++ src/pages/Movies/UpcomingMovies/UpcomingMoviesPage.tsx
@@ -0,0 +1,34 @@
+import { toast } from "react-toastify";
+import Loader from "../../../components/ui/Loader/Loader";
+import { useFilterUpcomingMovies } from "../../../hooks/filters/useSiltersMovies";
+import { useMovies } from "../../../hooks/movies/useMovies";
+import MovieCard from "../../../components/PageComponents/Movie/MovieCard";
+
+function UpcomingMovies() {
+  const { upcommingMoviesQuery } = useMovies();
+  const { data: upcommingMovies, isLoading, isError } = upcommingMoviesQuery;
+
+  const { filteredUpcomingMovies } = useFilterUpcomingMovies(
+    upcommingMovies?.results
+  );
+
+  if (isLoading) return <Loader />;
+  if (isError) return toast.error("No se pudo cargar la información");
+
+  return (
+    <section className="flex-1">
+      {filteredUpcomingMovies && (
+        <ul className="grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-5">
+          {filteredUpcomingMovies.length > 0 ? (
+            filteredUpcomingMovies.map((movie) => {
+              return <MovieCard movie={movie} key={movie.id} />;
+            })
+          ) : (
+            <li>No se encontró la información</li>
+          )}
+        </ul>
+      )}
+    </section>
+  );
+}
+export default UpcomingMovies;
