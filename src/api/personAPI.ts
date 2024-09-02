d592ba3f39331936a77fcbe2dae839f79677f779
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 16:33:26 -0500

    funcion para obtener las gente mas popular

--- src/api/personAPI.ts
+++ src/api/personAPI.ts
@@ -1,10 +1,26 @@
-import { MovieCreditsAPIResponse, PersonSchema } from "../schemas/movieSchema";
+import {
+  MovieCreditsAPIResponse,
+  PersonSchema,
+  PersonSearchSchema,
+} from "../schemas/movieSchema";
 import { Person } from "../types";
 import { api } from "./axios";
 
 export const getDetailsByPerson = async (id: Person["id"]) => {
   const { data } = await api(`/person/${id}`);
   const validateData = PersonSchema.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getPopularPerson = async () => {
+  const { data } = await api("/person/popular");
+  const validateData = PersonSearchSchema.safeParse(data);
+
+  console.log(validateData);
+  console.log(data);
 
   if (validateData.success) {
     return validateData.data;
