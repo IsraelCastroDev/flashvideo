0b4ac3b88e06e68f4c57f329ad5f0ce494f0a163
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 16:39:59 -0500

    rendrizando el aside donde estan los filtros segun el pathname

--- src/layouts/LayoutPage.tsx
+++ src/layouts/LayoutPage.tsx
@@ -1,16 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/ui/Header/Header";
 import Filters from "../components/Filters/Filters";
 import Footer from "../components/Footer";
 
 function LayoutPage() {
+  const location = useLocation();
+  const isPersonPage = location.pathname.startsWith("/person");
+
   return (
     <>
       <Header />
-      <main className="flex flex-col md:flex-row gap-4 px-4 md:px-10 py-2 md:py-7 min-h-[calc(100vh-80px)]">
-        <Filters />
-        <Outlet />
-      </main>
+      {isPersonPage ? (
+        <main className="flex flex-col gap-4 px-4 md:px-10 py-2 md:py-7 min-h-[calc(100vh-80px)]">
+          <Outlet />
+        </main>
+      ) : (
+        <main className="flex flex-col md:flex-row gap-4 px-4 md:px-10 py-2 md:py-7 min-h-[calc(100vh-80px)]">
+          <Filters />
+          <Outlet />
+        </main>
+      )}
       <Footer />
     </>
   );
