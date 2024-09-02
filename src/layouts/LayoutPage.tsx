0fab629dc30acca6d11946d24144971a4e8d372c
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 13:29:13 -0500

    ajustando css

--- src/layouts/LayoutPage.tsx
+++ src/layouts/LayoutPage.tsx
@@ -7,7 +7,7 @@ function LayoutPage() {
   return (
     <>
       <Header />
-      <main className="flex flex-col md:flex-row gap-4 px-4 md:px-10 py-2 md:py-7">
+      <main className="flex flex-col md:flex-row gap-4 px-4 md:px-10 py-2 md:py-7 min-h-[calc(100vh-80px)]">
         <Filters />
         <Outlet />
       </main>
