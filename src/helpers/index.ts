825ca8f2252c6cae43edc8373c63155f8f1f8f23
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 14:37:38 -0500

    sepaarando logica del hook en una funcion

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -61,3 +61,46 @@ export function addTypeToResults<
 >(results: T[], type_identifier: string): T[] & { type_identifier: string }[] {
   return results.map((result) => ({ ...result, type_identifier }));
 }
+
+export function filterMovies(
+  movies: Movie[] | undefined,
+  sort: string,
+  genreFilterId: number | null
+): Movie[] {
+  if (!movies) return [];
+
+  return movies
+    .slice() // Para crear una copia del array original antes de ordenar
+    .sort((a, b) => {
+      if (sort === "releaseDateAsc") {
+        return (
+          new Date(a.release_date ? a.release_date : new Date()).getTime() -
+          new Date(b.release_date ? b.release_date : new Date()).getTime()
+        );
+      }
+
+      if (sort === "releaseDateDesc") {
+        return (
+          new Date(b.release_date ? b.release_date : new Date()).getTime() -
+          new Date(a.release_date ? a.release_date : new Date()).getTime()
+        );
+      }
+
+      if (sort === "titleAZ") {
+        return a.title!.localeCompare(b.title ? b.title : "No title");
+      }
+
+      if (sort === "titleZA") {
+        return b.title!.localeCompare(a.title ? a.title : "No title");
+      }
+
+      return 0; // Si no coincide con ninguno de los filtros anteriores
+    })
+    .filter((movie) => {
+      if (genreFilterId) {
+        return movie.genre_ids?.includes(genreFilterId);
+      }
+
+      return true;
+    });
+}
