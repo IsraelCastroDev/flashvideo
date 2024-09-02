e52a5b467fac7b464cb6ad8c1104a222702b4aa4
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-02 16:33:54 -0500

    funciones para traer peliculas populares, mejor puntuadas, etc

--- src/api/tvSeries.ts
+++ src/api/tvSeries.ts
@@ -1,7 +1,9 @@
 import {
   CreditsAPIResponse,
+  GenreAPIResponse,
   TVSerieKeywordsResponse,
   TVSerieRecommendationsAPIResponse,
+  TVSeriesAPIResponse,
   TVSeriesSchema,
   VideosAPIResponse,
 } from "../schemas/movieSchema";
@@ -10,6 +12,42 @@ import { api } from "./axios";
 export const getTVSerie = async (id: number) => {
   const { data } = await api(`/tv/${id}`);
   const validateData = TVSeriesSchema.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getPopularTVSeries = async () => {
+  const { data } = await api("/tv/popular");
+  const validateData = TVSeriesAPIResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getTopRatedTVSeries = async () => {
+  const { data } = await api("/tv/top_rated");
+  const validateData = TVSeriesAPIResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getAiringTodayTVSeries = async () => {
+  const { data } = await api("/tv/airing_today");
+  const validateData = TVSeriesAPIResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getOnTheAirTVSeries = async () => {
+  const { data } = await api("/tv/on_the_air");
+  const validateData = TVSeriesAPIResponse.safeParse(data);
 
   if (validateData.success) {
     return validateData.data;
@@ -46,6 +84,15 @@ export const getTVSerieRecommendations = async (id: number) => {
 export const getTVSerieKeywords = async (id: number) => {
   const { data } = await api(`/tv/${id}/keywords`);
   const validateData = TVSerieKeywordsResponse.safeParse(data);
+
+  if (validateData.success) {
+    return validateData.data;
+  }
+};
+
+export const getGenres = async () => {
+  const { data } = await api("/genre/tv/list");
+  const validateData = GenreAPIResponse.safeParse(data);
 
   if (validateData.success) {
     return validateData.data;
